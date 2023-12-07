a5f8cb26f6161cd69cedf325240ad894cc13d537
Hans Yabao <hyabao0518@yahoo.com>
2023-12-07 11:57:40 -0500

    Written in routes and forms for registration and login pages

--- yaBlogApp/client/src/App.js
+++ yaBlogApp/client/src/App.js
@@ -1,31 +1,30 @@
 import './App.css';
+import Post from './Post';
+import Header from './Header';
+import Layout from "./Layout";
+import {Route,Routes} from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+
+
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="https://ya.ru" className="logo"> YaBlog </a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
+<Routes>
+
+  <Route path='/' element={<Layout />}>
+  <Route index element={<IndexPage /> }/> 
+  <Route path={'/login'} element={<LoginPage />} />
+  <Route path={'/register'} element={<RegisterPage />} />
 
 
-      <div className="post">
-        <div className="img">
-          <img src="https://s.yimg.com/ny/api/res/1.2/XzWJZqN2lND_X9D9SH378Q--/YXBwaWQ9aGlnaGxhbmRlcjt3PTI0MDA7aD0xNjAwO2NmPXdlYnA-/https://media.zenfs.com/en-us/homerun/complex.com/ad7bc3dbd951cec9c59ae7c647facf49" alt=""></img>
-        </div>
-        <div className="texts">
-          <h2> <a href=""> New clothing brand aims to target high calorie males. </a></h2>
-          <p className="info">
-            <a className="author">Hans Yabao</a>
-            <time>2023-01-06 16:45</time>
-          </p>
-          <p className="summary"> Popular clothing brand Levis...</p>
-        </div>
-      </div>
-    </main>
+
+  </Route>
+  
+
+</Routes>
+   
 
   );
 }
