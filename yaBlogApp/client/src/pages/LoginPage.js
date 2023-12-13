59f1d2a81654072556e338079674e2dd7a1b742a
Hans Yabao <hyabao0518@yahoo.com>
2023-12-13 17:34:12 -0500

    Added CSS to register and login pages

--- yaBlogApp/client/src/pages/LoginPage.js
+++ yaBlogApp/client/src/pages/LoginPage.js
@@ -1,10 +1,10 @@
 export default function LoginPage() {
     return (
-        <form action="">
+        <form className="login" >
+            <h1>Login</h1>
             <input type="text" placeholder="username"/>
             <input type="password" placeholder="password"/>
             <button>Login</button>
-
         </form>
     )
 }
